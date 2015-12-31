a93ac90d80996e49add19f58d37a38e3f779404a
dymodi <dymodi@gmail.com>
2015-12-31 21:45:48 +0800

    revised randomMPC

--- test_of_randomMPC.m
+++ test_of_randomMPC.m
@@ -5,10 +5,7 @@
 nu = 2;     % Number of inputs variables
 ny = 2;     % Number of outputs variables
 nx = 4;     % Number of states varaibles
-A = random(nx,nx);  % State transfer matrix;
-B = random(nx,nu);  % Input matrix;
-C = random(ny,nx);  % Outputs matrix
-D = zeros(ny,nu);   
+
 Ts = 0.05;          % Sampling time
 Nsim = 100;         % Simulation length
 P = 30;             % Prediction horizon
