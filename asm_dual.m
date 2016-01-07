3935d7e15bba773f93760c637b1fa40fdc1e3f11
dymodi <dymodi@gmail.com>
2016-01-07 17:40:09 +0800

    Dual ASM pass random QP tests. Although I am still not very clear of the mechanism.

--- asm_dual.m
+++ asm_dual.m
@@ -1,6 +1,6 @@
 % A Practical Dual Active Set Method
-% Dual algorithm based on Ferreau's thesis: An Online Active Set Strategy 
-% for Fast Solution of Parametric Quadratic Programs with Applications to 
+% Dual algorithm based on Ferreau's thesis: An Online Active Set Strategy
+% for Fast Solution of Parametric Quadratic Programs with Applications to
 % Predictive Engine Control
 % z = 1/2x'Hx + g'x, s.t. Gx>b
 % 2016.1.4
@@ -14,17 +14,18 @@
 
 function [xStar, iterStar, finalAS, failFlag] = asm_dual(H,invH,g,G,b,x,y,maxIter)
 
-%% Step 1 Initial primal and dual variable, intial active set
+%% Step 0 Initial primal and dual variable, intial active set
 [mc,ndec] = size(G);
 A = [];             % Current active set
 notA = (1:mc)';     % Current not active set
 actSize = 0;        % The cardinality of the current active set
 if isempty(x) || isempty(y)
     x = -invH*g;
-    y = zeros(mc,1);
+    y = [];
 end
 failFlag = 0;
-toStep3 = 0;
+toStep1 = 1;
+toStep2 = 0;
 
 % Iterations
 for k = 1:maxIter
@@ -33,15 +34,15 @@ for k = 1:maxIter
         error('Maximum iteration reached.');
     end
     
-    %% Step 2 Choose a violated (primal) constraint
-    if toStep3 == 0
+    %% Step 1 Choose a violated (primal) constraint
+    if toStep1 == 1 && toStep2 == 0
         optFlag = 1;
         for j = 1:length(notA)
             i = notA(j);
             if G(i,:)*x < b(i);    % Here we find a violated constraint
                 optFlag = 0;
                 q = i;             % Note, q is the id of the cons
-                qIndNotA = j;      
+                qIndNotA = j;
                 break;
             end
         end
@@ -52,9 +53,10 @@ for k = 1:maxIter
             finalAS = A;
             return;
         end
+        yPlus = [y;0];
     end
-
-    %% Step 3 Calculate step directions    
+    
+    %% Step 2 Calculate step directions
     GA = zeros(length(A),ndec);
     for j = 1:length(A)
         GA(j,:) = G(A(j),:);
@@ -62,21 +64,21 @@ for k = 1:maxIter
     Wk = inv(GA*invH*GA');
     Gq = G(q,:);
     
-    delta_x = -(invH*GA'*Wk*GA*invH-invH)*Gq';   % Note here is different 
-    % with the substance in the thesis    
+    delta_x = -(invH*GA'*Wk*GA*invH-invH)*Gq';   % Note here is different
+    % with the substance in the thesis
     delta_y = -Wk*GA*invH*Gq';  % Note, length(delta_y) = length(A);
     
     %% Step 4 Calculate the step length
-    if isZero(delta_x,1e-3)
+    if isZero(delta_x,1e-6)
         tauPrim = +Inf;
     else
-        tauPrim = -(Gq*x-b(q))/(Gq*delta_x);     % Note here is different 
-    % with the substance in the thesis    
+        tauPrim = -(Gq*x-b(q))/(Gq*delta_x);     % Note here is different
+        % with the substance in the thesis
     end
     tauDual = +Inf;
     for j = 1:length(A)
         if delta_y(j) < 0
-            tauTmp = -y(A(j))/delta_y(j);
+            tauTmp = -yPlus(j)/delta_y(j);
             if tauTmp < tauDual
                 tauDual = tauTmp;
                 blockJ = j;     % Note here j is the index of cons in A
@@ -89,60 +91,80 @@ for k = 1:maxIter
     end
     
     %% Step 5 Active set update
-    if isZero(delta_x,1e-3)
+    if isZero(delta_x,1e-6)
         if tauDual == Inf
             error('QP infeasible!');
         end
+        if length(yPlus) - length(delta_y) ~= 1
+            error('Dimension error.');
+        end
+        for j = 1:length(delta_y)
+            yPlus(j) = yPlus(j) + tau * delta_y(j);
+        end
+        yPlus(length(yPlus)) = yPlus(length(yPlus)) + tau;
         notA = [notA;A(blockJ)];    % Note that notA is not ordered
         A(blockJ) = [];
-        delta_y(blockJ) = [];
-        for i = 1:length(A)
-            y(A(i)) = y(A(i)) + tau * delta_y(i);
-            if y(A(i)) < 0
-                error('Dual infeasible!');
-            end
-        end
-        y(q) = y(q) + tau;
-        toStep3 = 1;
-        continue;
-    end
-    if tau == tauPrim
-        A = [A;q];
-        notA(qIndNotA) = [];
-        % A = sort(A);          % Do we need A to be ordered?
-        % Also note that if we force the order in A, then the new added
-        % cons q may not be located in the last position, thus the next
-        % for loop is wrong
-        x = x + tau * delta_x;  % Note that x keep original order
-        for i = 1:length(A)-1
-            y(A(i)) = y(A(i)) + tau * delta_y(i);
-            if y(A(i)) < 0
-                error('Dual infeasible!');
-            end
-        end
-        y(q) = y(q) + tau;
-        toStep3 = 0;
-    elseif tau == tauDual
-        notA = [notA;A(blockJ)];    % Note that notA is not ordered
-        A(blockJ) = [];
-        delta_y(blockJ) = [];
-        x = x + tau * delta_x;  % Note here is different with the thesis
-        % x = x + delta_x;  % This is Ferreau's version
-        for i = 1:length(A)
-            y(A(i)) = y(A(i)) + tau * delta_y(i);
-            if y(A(i)) < 0
-                error('Dual infeasible!');
-            end
-        end
-        y(q) = y(q) + tau;
-        toStep3 = 1;
+        y(blockJ) = [];
+        yPlus(blockJ) = [];
+        toStep1 = 0;
+        toStep2 = 1;
     else
-        error('tau error!');
-    end 
+        x = x + tau * delta_x;  % Note that x keep original order
+        if length(yPlus) - length(delta_y) ~= 1
+            error('Dimension error.');
+        end
+        for j = 1:length(delta_y)
+            yPlus(j) = yPlus(j) + tau * delta_y(j);
+            if yPlus(j) < -0.000001
+                error('Dual infeasible!');
+            end
+        end
+        yPlus(length(yPlus)) = yPlus(length(yPlus)) + tau;
+        if tau == tauPrim           % Full Step            
+            y = yPlus;
+            A = [A;q];
+            % A = sort(A);          % Do we need A to be ordered?
+            notA(qIndNotA) = [];
+            toStep1 = 1;
+            toStep2 = 0;
+        elseif tau == tauDual       % Partial Step
+            notA = [notA;A(blockJ)];    % Note that notA is not ordered
+            A(blockJ) = [];
+            y(blockJ) = [];
+            yPlus(blockJ) = [];
+            toStep1 = 0;
+            toStep2 = 1;
+        else
+            error('tau error!');
+        end
+        
+        % Check the primal variables with active set
+        GAt = zeros(length(A),ndec);
+        for j = 1:length(A)
+            GAt(j,:) = G(A(j),:);
+        end
+        bAt = zeros(length(A),1);
+        for j = 1:length(A)
+            bAt(j,:) = b(A(j),:);
+        end
+        if norm(GAt*x-bAt) > 1e-5
+            error('Active Set Error! Priaml Variables!');
+        end
+        
+        % Check the dual variables with active set
+        lambda = zeros(length(A),1);
+        for j = 1:length(A)
+            lambda(j,:) = y(j);
+        end
+        gt = H*x+g;
+        alambda = GAt'*lambda;
+        if norm(gt-alambda) > 1e-5
+            %error('Active Set Error! Dual Variables!');
+            disp('Active Set Error! Dual Variables!');
+        end
+    end
     
-    % Check the primal variables with active set
     
-    % Check the dual variables with active set
     
 end
 
